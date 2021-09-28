337af5151851234236351914b3506c5d46264464
Xeno Mannens <xenomannens@MacBook-Air-van-Xeno.local>
2021-09-28 16:25:12 +0200

    first commit

---
+++ Xeno_App/Xeno_App/Xeno_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xeno_AppApp.swift
+//  Xeno_App
+//
+//  Created by Xeno Mannens on 28/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Xeno_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
